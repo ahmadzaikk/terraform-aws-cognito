6d15f3fcbab0a7e87c7efba50a67c1a4f29b75bb
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-13 08:42:48 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "user_pool_id" {
 output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.this.id
 }
+
+output "user_pool_domain_url" {
+  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${var.aws_region}.amazoncognito.com"
+}
